f64ed30a0cc5a8474b52cbc756da6d6744d677fe
Tanesi Robledo <tanesi.robledo@student.tdsb.on.ca>
2025-04-27 20:50:52 -0400

    Added the featured section on the hero page

--- src/views/components/HeroSection.jsx
+++ src/views/components/HeroSection.jsx
@@ -1,7 +1,9 @@
 import './HeroSection.css'
-import './FeaturedSection.jsx'
 import Image1 from './Images/spider-verse-1280a-1542009456969_160w.jpg'
 import Image2 from './Images/Spider-Man-Into-The-Spider-Verse-Logo-PNG-Image.png'
+import FeaturedSection from './FeaturedSection';  // Adjust path as needed
+
+
 
 
 function HeroSection() {
@@ -9,27 +11,31 @@ function HeroSection() {
 
     return (
         <div className="container">
-        <div className="container-body">
-            <div className="sign-in-body">
-                <div className="intro-text">
-                    <h1>You Name It, We Got It! Stream Movies To Your Liking.</h1>
-                    <h3>More than 30,000 users have enjoyed our streaming services. Try now and begin you
-                        experience!</h3>
+            <div className="container-body">
+                <div className="sign-in-body">
+                    <div className="intro-text">
+                        <h1>You Name It, We Got It! Stream Movies To Your Liking.</h1>
+                        <h3>More than 30,000 users have enjoyed our streaming services. Try now and begin you
+                            experience!</h3>
+                    </div>
+                    {/* Featured Movies Grid */}
+                    <h2>Featured Now Playing</h2>
+                    <div className="featured-wrapper">
+                        <FeaturedSection />
+                    </div>
                 </div>
-
-            </div>
-            <div className="movie-image-container">
-                <div className="gradient"></div>
-                <img className="movie-image" src={Image1} alt=""/>
-                <img className="movie-title" src={Image2} alt=""/>
-                <div className="movie-info">
-                    <h3>After being bitten by a radioactive spider, Brooklyn teen Miles Morales gets a crash course
-                        in web-slinging from his alternate-dimension counterparts.</h3>
+                <div className="movie-image-container">
+                    <div className="gradient"></div>
+                    <img className="movie-image" src={Image1} alt="" />
+                    <img className="movie-title" src={Image2} alt="" />
+                    <div className="movie-info">
+                        <h3>After being bitten by a radioactive spider, Brooklyn teen Miles Morales gets a crash course
+                            in web-slinging from his alternate-dimension counterparts.</h3>
+                    </div>
+                    <button className="movie-button"><span className="movie-button-content">Watch Now</span></button>
                 </div>
-                <button className="movie-button"><span className="movie-button-content">Watch Now</span></button>
             </div>
-        </div>
-        {/* ERROR */}
+            {/* ERROR */}
         </div>
     )
 }
